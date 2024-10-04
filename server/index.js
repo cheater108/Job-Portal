4f6e02c0908dfc16945af65ee3d1c03dbec5a177
Chetan Anand <chetan108@outlook.com>
2024-10-05 00:11:03 +0530

    readme added

--- server/index.js
+++ server/index.js
@@ -19,10 +19,10 @@ app.use("/api/v1", indexRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist/index.html"));
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 app.listen(process.env.PORT, () => {
